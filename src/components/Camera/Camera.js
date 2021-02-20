74ae3cd8e0364d04009da94c2a14636215b81d3f
piotrrussw <piotr.wadycki@gmail.com>
2021-02-20 16:42:05 +0100

    color labels fix

--- src/components/Camera/Camera.js
+++ src/components/Camera/Camera.js
@@ -3,14 +3,17 @@ import CorrectionCamera from '~/components/Camera/CorrectionCamera';
 import BottomBar from '~/components/Camera/BottomBar';
 import styles from '~/components/Camera/Camera.module.scss';
 import ColorTrigger from '~/components/Camera/ColorTrigger';
+import { useStore } from '~/store';
 
 function Camera() {
+    const [{ showColorLabels }] = useStore();
+
     return (
         <main className={styles.container}>
             <TopBar />
             <CorrectionCamera />
             <BottomBar />
-            <ColorTrigger />
+            {showColorLabels && <ColorTrigger />}
         </main>
     );
 }
